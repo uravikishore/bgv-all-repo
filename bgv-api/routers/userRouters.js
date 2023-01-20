0a758db884d094104c6f8e39b2545b1db2eb51c0
unknown <uravikishore@gmail.com>
2023-01-20 13:27:17 +0530

    put add

--- bgv-api/routers/userRouters.js
+++ bgv-api/routers/userRouters.js
@@ -1,7 +1,7 @@
 const User = require("../models/users");
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const { models } = require("mongoose");
+// const { models } = require("mongoose");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -33,6 +33,34 @@ router.post("/", async (req, res) => {
 
   if (!user) {
     return res.status(400).send("User cannot be created :/");
+  }
+  res.send(user);
+});
+
+router.put("/:id", async (req, res) => {
+  const userInfo = await User.findById(req.params.id);
+
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password);
+  } else {
+    newPassword = userInfo.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      email: req.body.email,
+      passwordHash: newPassword,
+      isAdmin: req.body.isAdmin,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!user) {
+    return res.status(400).send("No user found :/");
   }
   res.send(user);
 });
